408bd37d918dd9e600d784f168906363f11ea165
apexdev <mwangi.em37@gmail.com>
2023-06-28 17:23:52 +0300

    chore: Connect to postgres
    Connect to postgres for the default Django base settings. Replace default connection to sqlite. Provision optional sqlite connection in 'dev.py' settings.

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,12 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { 'default': {env.db_url()} }
 
 
 # Password validation
