3bf4d2f4951955642752de396d330d6da2b68301
apexdev <mwangi.em37@gmail.com>
2024-03-25 12:36:26 +0300

    chore: Annotate dev/management entry-point script
    Problem:
    Errors raised by 'mypy' on unchecked script: 'manage.py'.
    
    Solution:
    Annotate the script with types and ignore specific cases.
    
    [Note]
    Given that 'manage.py' is not a submodule and lives in the project root,
    ignoring these errors could not be configured in the .ini 'mypy' config.

--- manage.py
+++ manage.py
@@ -4,11 +4,13 @@ import os
 import sys
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import (  # type: ignore[import-untyped]
+            execute_from_command_line,
+        )
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
