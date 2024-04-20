d5d3acbc4c1ff5ef617f7a9b1a7dc1f341b42f3b
apexdev <mwangi.em37@gmail.com>
2024-04-20 13:50:55 +0300

    refactor: Centralize (db) conn config
    Supply centralized DB config to both services.
    
    [Note]
    Use interpolation to provide values to Postgres-specific container ENVs.
    
    [Docs]
    Interpolation:
    https://docs.docker.com/compose/environment-variables/env-file/#interpolation
    
    [ABV]
    - conn: connection

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,11 +19,9 @@ env = environ.FileAwareEnv(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parents[2]
 
-# Take environment variables from .env file
+# Read environment variables from .env file
+environ.Env.read_env(Path(BASE_DIR, ".envs", "database.env"))
 environ.Env.read_env(Path(BASE_DIR, ".envs", "django.env"))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
@@ -85,7 +83,7 @@ DATABASES = {
         "HOST": env.str("DB_HOST"),
         "NAME": env.str("DB_NAME"),
         "PASSWORD": env("DB_PASSWORD"),
-        "PORT": env.str("DB_PORT"),
+        "PORT": env.int("DB_PORT"),
         "USER": env.str("DB_USER"),
     }
 }
