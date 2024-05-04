d05ca4dac78a8a838eb219e3a82f0ccb37e183a9
apexdev <mwangi.em37@gmail.com>
2024-05-04 18:10:31 +0300

    chore: Make (dev) default runtime environment
    Set `dev` settings as default runnable app config.
    
    [Patches]
    - Override core config required to run Django application.
    - Update docstrings
    
    [Note]
    - Temporarily disable the `PyFlakes` warning: `F405`.
      This warning will be resolved after complete segregated envs.

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,10 +1,18 @@
+# ruff: noqa: F405
 """Django `dev` settings for config project.
+
+Development target settings for the Django project inherited
+from the base environment common settings.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from .base import *  # noqa: F403
+
+# Core
+# https://docs.djangoproject.com/en/4.2/ref/settings/
+
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+DEBUG = env("DEBUG")
+SECRET_KEY = env("SECRET_KEY")
