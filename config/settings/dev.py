174b03b3187277d9fbc009fe538dae688e4a4cac
apexdev <mwangi.em37@gmail.com>
2024-05-14 09:58:12 +0300

    chore: Configure dev app (django-debug-toolbar)
    Follow the install process to integrate `djdt` in Django project.
    
    [Note]
    Configure and isolate app in `dev` target environment.
    
    [Docs]
    See, Installation:
    https://django-debug-toolbar.readthedocs.io/en/latest/installation.html

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,6 +8,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
 """
 
+import sys
+
+from config.settings.base import INSTALLED_APPS, MIDDLEWARE
 from config.settings.environment.database import DATABASES
 from config.settings.environment.django import (
     ALLOWED_HOSTS,
@@ -16,3 +19,26 @@ from config.settings.environment.django import (
 )
 
 from .base import *  # noqa: F403
+
+INTERNAL_IPS = [".localhost", "127.0.0.1", "[::1]"]
+
+# Include apps required only for development.
+INSTALLED_APPS.extend(
+    [
+        "debug_toolbar",
+    ]
+)
+
+# Include middleware required only for development.
+# Note: Consider middleware order is often critical to work effectively.
+MIDDLEWARE.extend(
+    [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+)
+
+
+# Only enable the toolbar when we're in debug mode and we're
+# not running tests. Django will change DEBUG to be False for
+# tests, so we can't rely on DEBUG alone.
+ENABLE_DEBUG_TOOLBAR = DEBUG and "test" not in sys.argv
