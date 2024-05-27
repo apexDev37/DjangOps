b35d38240de47e5e472f2d9d922ff4d4ab1fa143
apexdev <mwangi.em37@gmail.com>
2024-05-27 08:38:20 +0300

    chore: Configure (testing) env settings
    Add `TEST` setting to enable configuring created `tests` DB.
    Override default Django PWD hashers for faster hashing algorithms.
    
    [Note]
    Prefer `SHA1PasswordHasher` over `MD5PasswordHasher`, despite `MD5`
    being faster, `SHA1` better simulates a production-grade hasher.
    
    [TODO]
    Research and support running tests in separate parallel processes
    to increase tests runtime execution.
    
    [ABV]
    - PWD: password

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -5,12 +5,16 @@ Testing target settings for the Django project inherited
 from the base environment common settings.
 
 Note:
-    Django will change `DEBUG` to `False` when running tests
+    - Django will change `DEBUG` to `False` when running tests
     despite what has been configured in the environment.
+    - DB-dependent tests create isolated blank databases for test
+    execution only and do not use your “real” (production) database.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
 """
+
+import os
 
 from config.settings.environment.database import DATABASES
 from config.settings.environment.django import (
@@ -20,3 +24,21 @@ from config.settings.environment.django import (
 )
 
 from .base import *  # noqa: F403
+
+# Settings for test databases for the target env: `testing`.
+# NB: Test databases are destroyed when all the tests have been executed,
+# regardless of whether they (pass) or (fail).
+DATABASES["default"]["TEST"] = {"MIGRATE": True, "NAME": None}
+
+
+# TODO(apexDev37): Test running tests in separate parallel processes.
+# Uncomment after verified successful behavior.
+# os.environ.setdefault("DJANGO_TEST_PROCESSES", 4)  # noqa: ERA001
+
+
+# Leverage faster PWD hasher to speed up tests.
+# NB: (MD5) cryptographically weak hashing algorithm unsuitable for production.
+# NB: (SHA1) less faster, but more secure than `MD5`; significantly faster than Django defaults.
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.SHA1PasswordHasher",
+]
