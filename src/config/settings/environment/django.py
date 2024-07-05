7beb25e448b1ca8dfe968668f3220347ed18c0fa
apexdev <mwangi.em37@gmail.com>
2024-07-05 17:51:19 +0300

    refactor: Prefer enum over literal string
    Patches:
    - Prefer `Environment` enum over string literal
    - Add comment to document reason to explicitly not overwrite envs

--- src/config/settings/environment/django.py
+++ src/config/settings/environment/django.py
@@ -4,18 +4,24 @@ from pathlib import Path
 
 import environ
 
+from config.enums import Environment
 from config.settings.common.base import BASE_DIR
 
 # Set default project-wide ENV casting.
 env = environ.FileAwareEnv(
     ALLOWED_HOSTS=(list, []),
     DEBUG=(bool, False),
-    ENVIRONMENT=(str, "production"),
+    ENVIRONMENT=(str, Environment.PRODUCTION),
     SECRET_KEY=(str, None),
 )
 
-# Read environment variables from .env file
-environ.Env.read_env(Path(BASE_DIR, ".envs", "django.env"))
+# Read environment variables from .env file.
+# We explicitly do not want to override the above
+# envs where we also supply envs at the infra-level (Compose).
+environ.Env.read_env(
+    Path(BASE_DIR, ".envs", "django.env"),
+    overwrite=False,
+)
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 DEBUG = env("DEBUG")
