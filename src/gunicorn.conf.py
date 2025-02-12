3f2183e22ad484635b7be59bc52c877c6c11068a
apexdev <mwangi.em37@gmail.com>
2025-02-12 21:23:34 +0300

    refactor: Make (gunicorn) config dynamic
    Use ENVs to dynamically set values at infra/runtime level.
    Add Gunicorn example env file.
    
    [Reason]
    - Improve flexibility to modify config without incurring redeploy cost.
    - Abstract details that can infer host-machine related data.
    
    [Note]
    - Provide defaults within sensible ranges to ensure Gunicorn runs.

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -4,20 +4,62 @@ Optimize Gunicorn to handle HTTP requests efficiently. Leverage multiple workers
 and threads for concurrency. Set timeouts for graceful shutdowns, and log to
 stdout/stderr.
 
+Note:
+    - Set module-wide ENV casting and defaults.
+    - We define the casting here and not in `settings` for portability reasons
+      (ie. when Gunicorn config is located elsewhere, like `/etc/gunicorn/`).
+
 For more details on this module and available settings, see:
 https://docs.gunicorn.org/en/stable/settings.html
 """
 
 import multiprocessing
+from typing import TypeAlias
 
-bind = ["127.0.0.1:8000"]
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 3
-worker_class = "gthread"
-timeout = 120
-graceful_timeout = 30
-loglevel = "info"
-accesslog = "-"
-errorlog = "-"
-keepalive = 10
-preload_app = True
+import environ  # type: ignore[import-not-found]
+
+NUM_CORES: int = multiprocessing.cpu_count()
+
+env = environ.Env(  # type: ignore[misc]
+    GUNICORN_BIND=(list, ["0.0.0.0:8000"]),
+    # By default, allocate workers to CPU cores in a 1:1 ratio.
+    GUNICORN_WORKERS=(int, NUM_CORES),
+    GUNICORN_THREADS=(int, 2 * NUM_CORES),
+    GUNICORN_WORKER_CLASS=(str, "gthread"),
+    # For non-sync workers, `timeout` is not directly tied to the length of a
+    # single request. Workers responding to master process `check-ins` won't
+    # be killed due to a timeout.
+    GUNICORN_TIMEOUT=(int, 90),
+    GUNICORN_GRACEFUL_TIMEOUT=(int, 30),
+    GUNICORN_LOG_LEVEL=(lambda v: str(v).lower(), "info"),  # type: ignore[misc]
+    # Absolute path to log files.
+    # Examples: `/var/log/gunicorn/access.log`, `/var/log/gunicorn/error.log`
+    GUNICORN_ACCESS_LOG=(str, "-"),
+    GUNICORN_ERROR_LOG=(str, "-"),
+    # Match or exceed LB `keepalive` to upstream Gunicorn pm.
+    # Tune based on infra design, traffic patterns, and connection pool needs.
+    GUNICORN_KEEPALIVE=(int, 10),
+    GUNICORN_PRELOAD_APP=(bool, True),
+)
+
+# == Logging ===================================================================
+
+File: TypeAlias = str
+
+accesslog: File = env("GUNICORN_ACCESS_LOG")
+errorlog: File = env("GUNICORN_ERROR_LOG")
+loglevel: str = env("GUNICORN_LOG_LEVEL")
+
+# == Server (Mechanics) ========================================================
+bind: list[str] = env("GUNICORN_BIND")
+
+# == Server (Socket) ===========================================================
+preload_app: bool = env("GUNICORN_PRELOAD_APP")
+
+# == Workers ===================================================================
+workers: int = env("GUNICORN_WORKERS")
+worker_class: str = env("GUNICORN_WORKER_CLASS")
+threads: int = env("GUNICORN_THREADS")
+timeout: int = env("GUNICORN_TIMEOUT")
+graceful_timeout: int = env("GUNICORN_GRACEFUL_TIMEOUT")
+keepalive: int = env("GUNICORN_KEEPALIVE")
